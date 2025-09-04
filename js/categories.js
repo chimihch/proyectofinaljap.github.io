4ef51109b6da94b870832778f2a69d1f3270c78c
chimihch <isamar_d@proton.me>
2025-09-04 08:36:36 -0300

    cat to prot2

--- js/categories.js
+++ js/categories.js
@@ -37,7 +37,7 @@ function sortCategories(criteria, array){
 
 function setCatID(id) {
     localStorage.setItem("catID", id);
-    window.location = "products.html"
+    window.location = "prototipo2.html"
 }
 
 function showCategoriesList(){
